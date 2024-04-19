a42c08b824d0a76c92589ff8952c7887cabe2693
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-04-18 22:11:55 -0400

    created GithubRepo class

---
+++ server/lib/GithubRepo.js
@@ -0,0 +1,28 @@
+class GithubRepo {
+	constructor(
+		id,
+		name,
+		description,
+		private,
+		url,
+		homepage,
+		topics,
+		activity,
+		languages
+	) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.private = private;
+		this.url = new URL(url);
+		this.homepage = homepage;
+		this.topics = topics;
+		this.latestActivity = activity; // TODO: ensure the most recent activity is passed...
+		this.languages = languages; // TODO: test if API returns multiple languages
+	}
+
+	setSummary(readme) {
+		// TODO: parse the readme to create summary
+		return this.summary;
+	}
+}
