f2abd0e6b7ebc8a0ba2af49505b3262f5cf0f689
pbp66 <{ID}+{username}@users.noreply.github.com>
2024-04-19 17:00:58 -0400

    re-ordered Navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -13,18 +13,20 @@ export default function Navigation(props) {
 		>
 			<Container>
 				<div>
-					<h1 className="text-light">James Perry</h1>
+					<NavbarLink to="/aboutMe">
+						<h1 className="text-light">James Perry</h1>
+					</NavbarLink>
 				</div>
 				<div>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="navbar-nav">
 						<Nav className="me-auto">
 							<NavbarLink to="/">Featured</NavbarLink>
+							<NavbarLink to="/portfolio">Portfolio</NavbarLink>
+							<NavbarLink to="/Resume">Resume</NavbarLink>
+							<NavbarLink to="/contactMe">Contact Me</NavbarLink>
 							<NavbarLink to="/aboutMe">About Me</NavbarLink>
 							<NavbarLink to="/blog">Blog</NavbarLink>
-							<NavbarLink to="/portfolio">Portfolio</NavbarLink>
-							<NavbarLink to="/contactMe">Contact Me</NavbarLink>
-							<NavbarLink to="/Resume">Resume</NavbarLink>
 							<NavbarLink to="/vault">Vault</NavbarLink>
 						</Nav>
 					</Navbar.Collapse>
